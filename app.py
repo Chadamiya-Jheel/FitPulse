2c384db2ce4f41ff46a91687da9393e033d2299b
Chadamiya-Jheel <chadamiyajheel@gmail.com>
2026-07-07 15:27:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -549,3 +549,8 @@ if __name__ == "__main__":
         port  = int(os.getenv("PORT", 5000)),
         debug = os.getenv("FLASK_DEBUG", "0") == "1",
     )
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
